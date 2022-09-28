a66f916e6d63d2c184c4d152d761da01c5f242e5
Martijn Herber <m.herber@pl.hanze.nl>
2022-09-28 16:17:52 +0200

    Fully working version, giving the same results as Marcel's example

--- bed.py
+++ bed.py
@@ -9,19 +9,24 @@ def simple_pure_main(bedfile_name, pileup_name):
 
     chrms = defaultdict(OrderedDict)
     genes = {}
-    lowcov = []
-    
+
     for line in bed_fp:
         chrm, start, stop, gene = line.split(sep="\t")
+
+        try:            
+            genes[gene]['size'] += (int(stop) - int(start))+1
+        except KeyError:
+            genes[gene] = {'size' : int(stop) - int(start)+1,
+                           'low' : 0,
+                           'cov' : 0}
+
+                
         chrms["chr"+chrm].update({str(i) : gene for i in range(int(start), int(stop)+1)})
-        genes[gene] = {'size' : 0,
-                       'low' : 0,
-                       'cov' : 0}
+            
     for line in pileup_fp:
         chrm, pos, _, cov, _, _ = line.split("\t")
         try:
             genes[chrms[chrm][pos]]['cov'] += int(cov)
-            genes[chrms[chrm][pos]]['size'] += 1
             if int(cov) < 30: genes[chrms[chrm][pos]]['low'] += 1
         except KeyError:
             pass
@@ -29,7 +34,7 @@ def simple_pure_main(bedfile_name, pileup_name):
     report_fp = open("report.txt", "w")
     report_fp.write("GENE\tSIZE\tAVG.Coverage\tLow.Positions\n")
     for gene in genes:
-        report_fp.write(f"{gene[:-1]}\t{genes[gene]['size']}\t{genes[gene]['cov'] / genes[gene]['size']}\t{genes[gene]['low']}\n")
+        report_fp.write(f"{gene[:-1]}\t{genes[gene]['size']}\t{round(genes[gene]['cov'] / genes[gene]['size'],1)}\t{genes[gene]['low']}\n")
     report_fp.close()
     return 0
     
